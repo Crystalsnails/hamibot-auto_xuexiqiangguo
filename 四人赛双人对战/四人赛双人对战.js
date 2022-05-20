18c81d572ce877e6af3aebe949f7581d4c0323c6
McMug2020 <92876629+McMug2020@users.noreply.github.com>
2022-05-20 14:14:49 +0800

    Update 四人赛双人对战.js

--- 四人赛双人对战/四人赛双人对战.js
+++ 四人赛双人对战/四人赛双人对战.js
@@ -161,32 +161,6 @@ function my_click_clickable(target) {
   }
 }
 
-/**
- * 处理访问异常
- */
-function handling_access_exceptions() {
-  if (text("访问异常").exists()) {
-    // 滑动按钮位置
-    var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
-    // 滑动框右边界
-    var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
-    // 位置取随机值
-    var randomX = random(pos.left, pos.right);
-    var randomY = random(pos.top, pos.bottom);
-    swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
-  }
-}
-
-/* 
-处理访问异常，滑动验证
-*/
-// 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
-var id_handling_access_exceptions;
-var thread_handling_access_exceptions = threads.start(function () {
-  // 每2秒就处理访问异常
-  id_handling_access_exceptions = setInterval(handling_access_exceptions, 2000);
-});
-
 /** 
  * 选出选项
  * @param {answer} answer 答案
@@ -470,6 +444,22 @@ function ocr_processing(text, if_question) {
 */
 
 /**
+ * 处理访问异常
+ */
+function handling_access_exceptions() {
+  if (text("访问异常").exists()) {
+    // 滑动按钮位置
+    var pos = className('android.view.View').depth(10).clickable(true).findOnce(1).bounds();
+    // 滑动框右边界
+    var right_border = className('android.view.View').depth(9).clickable(false).findOnce(0).bounds().right;
+    // 位置取随机值
+    var randomX = random(pos.left, pos.right);
+    var randomY = random(pos.top, pos.bottom);
+    swipe(randomX, randomY, randomX + right_border, randomY, random(200, 400));
+  }
+}
+
+/**
 * 答题
 */
 function do_contest() {
@@ -532,6 +522,16 @@ if (!className('android.view.View').depth(21).text('学习积分').exists()) {
   my_click_clickable('我的');
   my_click_clickable('学习积分');
 }
+
+/* 
+处理访问异常，滑动验证
+*/
+var id_handling_access_exceptions;
+// 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
+var thread_handling_access_exceptions = threads.start(function () {
+  // 每2秒就处理访问异常
+  id_handling_access_exceptions = setInterval(handling_access_exceptions, 2000);
+});
 
 /*
 **********四人赛*********
