1182feaac0d3fe47652f4acff4223fd2d474fe89
McMug2020 <92876629+McMug2020@users.noreply.github.com>
2022-06-04 00:05:55 +0800

    Update 学习强国.js

--- 学习强国.js
+++ 学习强国.js
@@ -28,11 +28,11 @@ storage.remove("answer_question_map");
 storage.remove("answer_question_map1");
 var date = new Date();
 // 每周五定时更新题库，周日为0
-var update_day = date.getDay();
-if (update_day == 5) storage.remove('answer_question_map2');
+//var update_day = date.getDay();
+//if (update_day == 5) storage.remove('answer_question_map2');
 // 或设定每月某日定时更新
-//var update_day = date.getDate();
-//if (update_day == 3) storage.remove('answer_question_map2');
+var update_day = date.getDate();
+if (update_day == 3) storage.remove('answer_question_map2');
 
 delay_time = Number(delay_time) * 1000;
 
