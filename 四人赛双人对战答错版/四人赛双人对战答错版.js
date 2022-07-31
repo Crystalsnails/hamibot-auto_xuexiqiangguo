66a8fbb866937ea6bda126c07b3d4ec1ff9e6b20
McMug2020 <92876629+McMug2020@users.noreply.github.com>
2022-07-31 22:31:44 +0800

    Update 四人赛双人对战答错版.js

--- 四人赛双人对战答错版/四人赛双人对战答错版.js
+++ 四人赛双人对战答错版/四人赛双人对战答错版.js
@@ -19,7 +19,7 @@ function random_time(time) {
 
 function entry_model(number) {
     sleep(random_time(delay_time * 2));
-    var model = className('android.view.View').depth(22).findOnce(number);
+    var model = className("android.view.View").depth(22).findOnce(number);
     while (!model.child(3).click());
 }
 
@@ -27,8 +27,8 @@ function entry_model(number) {
 function my_click_clickable(target) {
     text(target).waitFor();
     // 防止点到页面中其他有包含“我的”的控件，比如搜索栏
-    if (target == '我的') {
-        id('comm_head_xuexi_mine').findOne().click();
+    if (target == "我的") {
+        id("comm_head_xuexi_mine").findOne().click();
     } else {
         click(target);
     }
@@ -41,13 +41,13 @@ function handling_access_exceptions() {
     // 在子线程执行的定时器，如果不用子线程，则无法获取弹出页面的控件
     var thread_handling_access_exceptions = threads.start(function() {
         while (true) {
-            textContains('访问异常').waitFor();
+            textContains("访问异常").waitFor();
             // 滑动按钮“>>”位置
-            idContains('nc_1_n1t').waitFor();
-            var bound = idContains('nc_1_n1t').findOne().bounds();
+            idContains("nc_1_n1t").waitFor();
+            var bound = idContains("nc_1_n1t").findOne().bounds();
             // 滑动边框位置
-            text('向右滑动验证').waitFor();
-            var slider_bound = text('向右滑动验证').findOne().bounds();
+            text("向右滑动验证").waitFor();
+            var slider_bound = text("向右滑动验证").findOne().bounds();
             // 通过更复杂的手势验证（向右滑动过程中途停顿）
             var x_start = bound.centerX();
             var dx = x_start - slider_bound.left;
@@ -59,12 +59,12 @@ function handling_access_exceptions() {
             x_end = random(x_end, x_end + 10);
             gesture(random(delay_time, delay_time + 50), [x_start, y_start], [x_mid, y_end], [x_end, y_end]);
             sleep(delay_time / 2);
-            if (textContains('刷新').exists()) {
-                click('刷新');
+            if (textContains("刷新").exists()) {
+                click("刷新");
                 continue;
             }
-            if (textContains('网络开小差').exists()) {
-                click('确定');
+            if (textContains("网络开小差").exists()) {
+                click("确定");
                 continue;
             }
             // 执行脚本只需通过一次验证即可，防止占用资源
@@ -80,49 +80,49 @@ function handling_access_exceptions() {
 var thread_handling_access_exceptions = handling_access_exceptions();
 
 function do_it() {
-    while (!text('开始').exists());
-    while (!text('继续挑战').exists()) {
+    while (!text("开始").exists());
+    while (!text("继续挑战").exists()) {
         sleep(random_time(contest_delay_time));
         // 随机选择
         try {
-            var options = className('android.widget.RadioButton').depth(32).find();
+            var options = className("android.widget.RadioButton").depth(32).find();
             var select = random(0, options.length - 1);
-            className('android.widget.RadioButton').depth(32).findOnce(select).click();
+            className("android.widget.RadioButton").depth(32).findOnce(select).click();
         } catch (error) {
         }
-        while (!textMatches(/第\d题/).exists() && !text('继续挑战').exists() && !text('开始').exists());
+        while (!textMatches(/第\d题/).exists() && !text("继续挑战").exists() && !text("开始").exists());
     }
 }
 
-if (!className('android.view.View').depth(21).text('学习积分').exists()) {
-    app.launchApp('学习强国');
+if (!className("android.view.View").depth(21).text("学习积分").exists()) {
+    app.launchApp("学习强国");
     sleep(random_time(delay_time * 3));
     var while_count = 0;
-    while (!id('comm_head_title').exists() && while_count < 5) {
+    while (!id("comm_head_title").exists() && while_count < 5) {
         while_count++;
         back();
         sleep(random_time(delay_time));
     }
-    app.launchApp('学习强国');
+    app.launchApp("学习强国");
     sleep(random_time(delay_time));
-    my_click_clickable('我的');
-    my_click_clickable('学习积分');
+    my_click_clickable("我的");
+    my_click_clickable("学习积分");
 }
 
 /*
 **********四人赛*********
 */
-if (four_player_battle == 'yes') {
+if (four_player_battle == "yes") {
     sleep(random_time(delay_time));
-    className('android.view.View').depth(21).text('学习积分').waitFor();
+    className("android.view.View").depth(21).text("学习积分").waitFor();
     entry_model(10);
     for (var i = 0; i < four_player_count; i++) {
         sleep(random_time(delay_time));
-        my_click_clickable('开始比赛');
+        my_click_clickable("开始比赛");
         do_it();
         if (i < four_player_count - 1) {
             sleep(random_time(delay_time));
-            while (!click('继续挑战'));
+            while (!click("继续挑战"));
             sleep(random_time(delay_time));
         }
     }
@@ -135,25 +135,25 @@ if (four_player_battle == 'yes') {
 /*
 **********双人对战*********
 */
-if (two_player_battle == 'yes') {
+if (two_player_battle == "yes") {
     sleep(random_time(delay_time));
-    className('android.view.View').depth(21).text('学习积分').waitFor();
+    className("android.view.View").depth(21).text("学习积分").waitFor();
     entry_model(11);
 
     for (var i = 0; i < two_player_count; i++) {
         // 点击随机匹配
-        text('随机匹配').waitFor();
+        text("随机匹配").waitFor();
         sleep(random_time(delay_time * 2));
         try {
-            className('android.view.View').clickable(true).depth(24).findOnce(1).click();
+            className("android.view.View").clickable(true).depth(24).findOnce(1).click();
         } catch (error) {
-            className('android.view.View').text('').findOne().click();
+            className("android.view.View").text("").findOne().click();
         }
         do_it();
         sleep(random_time(delay_time));
         if (i < two_player_count - 1) {
             sleep(random_time(delay_time));
-            while (!click('继续挑战'));
+            while (!click("继续挑战"));
             sleep(random_time(delay_time));
         }
     }
@@ -161,10 +161,10 @@ if (two_player_battle == 'yes') {
     back();
     sleep(random_time(delay_time));
     back();
-    my_click_clickable('退出');
+    my_click_clickable("退出");
 }
 
 // 震动半秒
 device.vibrate(500);
-toast('脚本运行完成');
+toast("脚本运行完成");
 exit();
